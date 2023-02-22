11b2f595ec75dfdf4799169ab212a01516510e8b
sixca <sixca0520@gmail.com>
2023-02-22 23:00:34 +0900

    task final commit

--- day02/src/day02/P55.java
+++ day02/src/day02/P55.java
@@ -1,16 +1,37 @@
 package day02;
 
 public class P55 {
+// 타입변환 과제
+ 	  public static void main(String[] args) { 
+		   int a = 2000000000; 
+		   int b = 2000000000; 
+		  
+		   Long aLong= new Long(a);
+		   Long bLong= new Long(b);
+		   System.out.print(aLong + bLong);
+		  
+		  } 
 
-//case.1	  
+//case.2
+ 	   /* public static void main(String[] args) { 
+		   int a = 2000000000; 
+		   int b = 2000000000; 
+		  
+		   Long aLong= Long.valueOf(a);
+		   Long bLong= Long.valueOf(b);
+		   System.out.print(aLong + bLong);
+		  
+		  } */
+ 	  
+//case.3	  
 	  /*public static void main(String[] args) { 
 	   int a = 2000000000; 
 	   int b = 2000000000; 
-	   System.out.println((long)a + b);
+	   System.out.print((long)a + b);
 	  
-	  } */
-	
-//case.2	 
+	  }  */	  
+ 	  
+//case.4	 
 	/*public static void main(String[] args) {     
 		int a = 2000000000;                      
 		int b = 2000000000;                      
@@ -18,14 +39,14 @@ public class P55 {
                                                  
 	} */   
 	
- //case.3
-	public static void main(String[] args) {  
+//case.5
+	/*public static void main(String[] args) {  
   		int a = 2000000000;                   
 		int b = 2000000000;                   
    		long c = a;                           
 		long d = b;                           
       	System.out.print(c + d);              
                                                    
-     }      
+     }   */   
 	
  }
