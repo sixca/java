92d4e1b27bb8c919a71b1b8f6bcd7de98baf630a
sixca <sixca0520@gmail.com>
2023-02-22 19:18:31 +0900

    20230222 task updated

--- day02/src/day02/P55.java
+++ day02/src/day02/P55.java
@@ -2,11 +2,30 @@ package day02;
 
 public class P55 {
 
-	public static void main(String[] args) {
-		int a = 20;
-		int b = 20;
-		System.out.println(a + b + "억");
-
-	}
-
-}
+//case.1	  
+	  /*public static void main(String[] args) { 
+	   int a = 2000000000; 
+	   int b = 2000000000; 
+	   System.out.println((long)a + b);
+	  
+	  } */
+	
+//case.2	 
+	/*public static void main(String[] args) {     
+		int a = 2000000000;                      
+		int b = 2000000000;                      
+		System.out.printf("%.0f", (double)a + b);
+                                                 
+	} */   
+	
+ //case.3
+	public static void main(String[] args) {  
+  		int a = 2000000000;                   
+		int b = 2000000000;                   
+   		long c = a;                           
+		long d = b;                           
+      	System.out.print(c + d);              
+                                                   
+     }      
+	
+ }
