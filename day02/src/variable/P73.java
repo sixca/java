510edbddf95d2b8345a8a978c2e9a8d9f2fc571d
sixca <sixca0520@gmail.com>
2023-02-23 17:48:40 +0900

    230223 last pang

--- day02/src/variable/P73.java
+++ day02/src/variable/P73.java
@@ -6,10 +6,6 @@ public class P73 {
 
 	public static void main(String[] args) {
 
-		int a = 0;
-
-		String str = "";
-       
 		Scanner sc = new Scanner(System.in);
 		System.out.println("명령어를 입력 하시오(q,i,d,s)");
 		String cmd = sc.next();
