b96751cf9f24fca498e7f2d4530a35bd5ee7fdd6
sixca <sixca0520@gmail.com>
2023-02-27 09:34:50 +0900

    rulu commit2

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,6 +4,7 @@ public class P99 {
 //구구단즈
 			public static void main(String[] args) {
 				int b = 200;
+				int a = 10;
 				for(int i=2;i<10;i++) {
 					System.out.printf("%d 단 \n",i);
 					for(int j=1;j<10;j++) {
