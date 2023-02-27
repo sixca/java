dfaabf4146080710021ee0532e3a72115b7cc56f
sixca <sixca0520@gmail.com>
2023-02-27 16:08:22 +0900

    0227

--- day03/src/array/P122.java
+++ day03/src/array/P122.java
@@ -3,31 +3,34 @@ package array;
 import java.util.Scanner;
 
 public class P122 {
+	
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			while (true) {
+				System.out.println("Input Commend[q,a1,a2,a3,a4]");
+				String cmd = sc.next();
+				if (cmd.equals("q")) {
+					System.out.println("Quit..");
+					break;
+				} else if (cmd.equals("a1")) {
+					System.out.println("Select..");
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while(true) {       //계속 반복
-			System.out.println("Input Command[q,i,s]");
-			String cmd = sc.next();
-			if(cmd.equals("q")) {
-				System.out.println("Quit..");
-				break;
-				
-			}else if (cmd.equals("a1")) {
-				
-			}else if (cmd.equals("a2")) {
-			
-			}else if (cmd.equals("a3")) {
-				
-			}else if (cmd.equals("a4")) {
-				
-			}else {
-				System.out.println("Invalid Command..");
-				System.out.println("Try Again");
-			}
-		}  // end while
-		System.out.println("Bye...");
-		sc. close();
+				} else if (cmd.equals("a2")) {
+					System.out.println("Select..");
+
+				} else if (cmd.equals("a3")) {
+					System.out.println("Select..");
+
+				} else if (cmd.equals("a4")) {
+					System.out.println("Select..");
+
+				} else {
+					System.out.println("Invalid Command..");
+					System.out.println("Tri Again..");
+				}
+			} // end while
+			System.out.println("Bye..");
+			sc.close();
+		}
+
 	}
-
-}
