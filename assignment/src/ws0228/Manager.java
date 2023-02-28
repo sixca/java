a407c5a9907a5a7eb09f8a7282a67b24e1b7b7b6
sixca <sixca0520@gmail.com>
2023-02-28 18:35:26 +0900

    0228 WS commit

---
+++ assignment/src/ws0228/Manager.java
@@ -0,0 +1,65 @@
+package ws0228;
+
+public class Manager {
+	private String id;
+	private String name;
+	private int salary;
+	private int bonus;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public Manager() {
+		this.id = "temp0001";
+		this.name = "temp";
+		this.salary = 10000000;
+		this.bonus = 1000000;
+	}
+
+	public Manager(String id, String name, int salary, int bonus) {
+		this.id = id; // 여기선this가 필수인 상황. this빼니깐 has no effect뜨네. 다른 곳에서는 다 this는 생략가능
+		this.name = name;
+		if (salary < 0) {
+			this.salary = 0;
+		} else {
+			this.salary = salary;
+		}
+		if (bonus < 0) {
+			this.bonus = 0;
+		} else {
+			this.bonus = bonus;
+		}
+	}
+
+	public Manager(String name, int salary, int bonus) {
+		this("10000", name, salary, bonus); // 위 Manager 컨스트럭터를 호출. P143~144
+
+	}
+
+	public int getAnnSalary() {
+		return this.salary * 12 + bonus; // 만들어질 객체의 ~ 'this'
+	}
+
+	public double getTax() {
+		return this.getAnnSalary() * 0.175;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [id=" + id + ", name=" + name + ", salary=" + salary + ", bonus=" + bonus + "]";
+	}
+
+}
---
+++ assignment/src/ws0228/app.java
@@ -0,0 +1,74 @@
+package ws0228;
+
+public class app {
+
+	public static void main(String[] args) {
+		// employee 5명, manager 5명 배열 지정
+
+		Employee[] arrEmp = { 
+				new Employee("0001", "Emp1", 1000000), 
+				new Employee("0002", "Emp2", 1100000),
+				new Employee("0003", "Emp3", 1200000), 
+				new Employee("0004", "Emp4", 1300000),
+				new Employee("0005", "Emp5", 1400000), };
+		Manager[] arrMag = { 
+				new Manager("1001", "Mag1", 2000000, 200000), 
+				new Manager("1002", "Mag2", 2100000, 210000),
+				new Manager("1003", "Mag3", 2200000, 220000), 
+				new Manager("1004", "Mag4", 2300000, 230000),
+				new Manager("1005", "Mag5", 2400000, 240000), };
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (i == 0) {
+					System.out.println(arrEmp[j].toString());
+				} else {
+					System.out.println(arrMag[j].toString());
+				}
+			}
+			System.out.println("");
+
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (i == 0) {
+					System.out.println(arrEmp[j].getName() 
+							+ "의 연봉은 " 
+							+ arrEmp[j].getAnnSalary() 
+							+ "원이고, 납부할 세금은 "
+							+ Math.round(arrEmp[j].getTax()) 
+							+ "원입니다."
+							);
+				} else {
+					System.out.println(arrMag[j].getName() 
+							+ "의 연봉은 " 
+							+ arrMag[j].getAnnSalary() 
+							+ "원이고, 납부할 세금은 "
+							+ Math.round(arrMag[j].getTax()) 
+							+ "원입니다.");
+				}
+			}
+			System.out.println("");
+
+		}
+
+		for (int i = 0; i < 2; i++) {
+			int sum = 0;
+			for (int j = 0; j < 5; j++) {
+				if (i == 0) {
+					sum += arrEmp[j].getAnnSalary();
+					if (j == 4) {
+						System.out.printf("Employee의 연봉 합계: %s원 \n", sum);
+					}
+				} else {
+					sum += arrMag[j].getAnnSalary();
+					if (j == 4) {
+						System.out.printf("Manager의 연봉 합계: %s원", sum);
+					}
+				}
+			}
+
+		}
+
+	}
+}
