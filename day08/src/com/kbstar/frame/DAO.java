ef6d54fa17079c20ac1df995c7a1c8af3c037c63
sixca <sixca0520@gmail.com>
2023-03-08 09:14:23 +0900

    updated

--- day08/src/com/kbstar/frame/DAO.java
+++ day08/src/com/kbstar/frame/DAO.java
@@ -1,7 +1,7 @@
 package com.kbstar.frame;
 
 public interface DAO<K, V> {        //K타입이 들어오고, V타입이 들어올 것이라 미리 예상해서 정의하는 것 "제네릭"
-	public static final String ip = "127.0.0.1";  // 상수, public static final은 생략 가능
+	public static final String IP = "127.0.0.1";  // 상수, public static final은 생략 가능
 			
 	public void insert(V v);   //추상메서드   :: public abstract void 임. abstract는 생략됨.
 	public void delete(K k);
@@ -9,9 +9,9 @@ public interface DAO<K, V> {        //K타입이 들어오고, V타입이 들어
 	
 	//디폴트 메서드
 	default void connect() {
-		System.out.println(ip+"에 접속하였습니다.");
+		System.out.println(IP+"에 접속하였습니다.");
 	}
 	default void close() {
-		System.out.println(ip+"에 접속을 해제 하였습니다.");
+		System.out.println(IP+"에 접속을 해제 하였습니다.");
 	}
 }
